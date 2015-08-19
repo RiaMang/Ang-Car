e87e57fd7c11eefddd44f4e49a323014ad61830d
RiaMang <1riamanglani@gmail.com>
2015-08-19 13:59:57 -0400

    basic stuff

---
+++ app/indexCtrl.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var app = angular.module('car-finder');
+    app.controller('indexCtrl',['$interval', function ($interval) {
+        var self = this;
+        this.value = 1;
+        this.time = new Date();
+
+        $interval(function () {
+            self.time = new Date();
+        },100);
+    }]);
+})();
